d11504c865321ad95dc4d3b03af132bc0ae38bec
Omich <omich.kun@gmail.com>
2019-03-06 11:06:08 +0300

    changed calls hash to array

--- lib/freeswitch_log_parser.rb
+++ lib/freeswitch_log_parser.rb
@@ -60,59 +60,54 @@ module FreeswitchLogParser
 		end
 
 
-		calls = {}
-		uuids = []
+		calls = []
 
 		line_number = 1
 		lines.each do |line|
 			
 			if /New Channel/.match(line)
 				new_call = create_call(line, line_number)
-				uuids << new_call.uuid
-				calls[new_call.uuid] = new_call
+				calls << new_call
 			end
 
 			uuid = find_uuids(line).first
 
-			uuids.each do |uuid|
-				# check all lines for depending from some call
-				if Regexp.union(uuid).match line
-					call = calls.values.select {|c| c.uuid == uuid}.first
-					call.lines << line
-					
-					# Define abonent if it exists
-					if /(?<=\[)INFO(?=\])/.match(line) && /Processing/.match(line)
-						call.abonent_b = /(?<=\-\>)(.+)(?=\s)/.match(line).to_s.split(" ")[0].strip
-					end
+			call = calls.select {|c| c.uuid == uuid}.first
+			if call
+				call.lines << line
+				
+				# Define abonent B if it exists
+				if /(?<=\[)INFO(?=\])/.match(line) && /Processing/.match(line)
+					call.abonent_b = /(?<=\-\>)(.+)(?=\s)/.match(line).to_s.split(" ")[0].strip
+				end
 
-					# Define end of the call
-					if /State DESTROY going to sleep/.match(line)
-						call.time_end = Time.parse(/\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2}\.\d{6}/.match(line).to_s)
-						call.line_end = line_number
-						call.call_length = call.time_end - call.time_start 
-						uuids.delete(uuid)
-					end
+				# Define end of the call
+				if /State DESTROY going to sleep/.match(line)
+					call.time_end = Time.parse(/\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2}\.\d{6}/.match(line).to_s)
+					call.line_end = line_number
+					call.call_length = call.time_end - call.time_start 
+				end
 
-					# Get PASS routes 
-					if /Regex \(PASS\)/.match(line)
-						call.routes_pass_lines << line
-						call.routes_pass_names << /\[(.*?)\]/.match(line).to_s
-					end
+				# Get PASS routes 
+				if /Regex \(PASS\)/.match(line)
+					call.routes_pass_lines << line
+					call.routes_pass_names << /\[(.*?)\]/.match(line).to_s
+				end
 
-					if /Action/.match(line)
-						call.actions << line
-					end
-					calls[uuid] = call
+				if /Action/.match(line)
+					call.actions << line
 				end
 			end
+
+
 			line_number += 1
 		end
 
-		calls.each do |call_key, call_value|
-			calls.delete(call_key) if call_value.abonent_b == nil
+		calls.each do |call|
+			calls.delete(call) if call.abonent_b == nil
 		end
 
-		calls.each_value  do |call|	
+		calls.each  do |call|	
 			puts  "#{call.line_start} #{call.uuid}: #{call.abonent_a} -> #{call.abonent_b} #{call.call_length}s"
 		end
 	end
